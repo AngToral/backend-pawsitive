b967fcd8b0fbecc09e42cb08750f2eb390f3e85a
Angela Toral <158821520+AngToral@users.noreply.github.com>
2025-05-13 19:13:40 +0200

    all dependencies installed

---
+++ index.js
@@ -0,0 +1,27 @@
+require('dotenv').config()
+
+const express = require('express')
+const app = express()
+const cors = require('cors')
+const port = process.env.PORT || 3000
+
+
+
+const mongoose = require("mongoose")
+const mongoDB = "mongodb+srv://" + process.env.DB_USER + ":" + process.env.DB_PASSWORD + "@" + process.env.DB_SERVER + "/" + process.env.DB_NAME + "?retryWrites=true&w=majority";
+async function main() {
+    await mongoose.connect(mongoDB);
+}
+main().catch(err => console.log(err));
+
+app.use(cors())
+app.use(express.json())
+
+
+
+const server = app.listen(port, () => {
+    console.log(`Example app listening on port ${port}`);
+    console.log("The database user is: ", process.env.DB_USER)
+});
+
+module.exports = { app, server };
