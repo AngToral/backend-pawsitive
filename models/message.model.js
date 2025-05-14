1532bf0f4440f710841299e4e4ca3f0d1e5a0561
Angela Toral <158821520+AngToral@users.noreply.github.com>
2025-05-14 18:38:39 +0200

    message model

---
+++ models/message.model.js
@@ -0,0 +1,35 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const messageSchema = new Schema({
+    conversation: {
+        type: Schema.Types.ObjectId,
+        ref: 'conversation',
+        required: true
+    },
+    sender: {
+        type: Schema.Types.ObjectId,
+        ref: 'userModel',
+        required: true
+    },
+    text: String,
+    image: String,
+    read: {
+        type: Boolean,
+        default: false
+    },
+    readBy: [{
+        user: {
+            type: Schema.Types.ObjectId,
+            ref: 'userModel'
+        },
+        readAt: {
+            type: Date,
+            default: Date.now
+        }
+    }]
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('message', messageSchema);
