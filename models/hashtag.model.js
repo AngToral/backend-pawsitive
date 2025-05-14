3fe9d476365bd3a685baf41477419ac5516e57ce
Angela Toral <158821520+AngToral@users.noreply.github.com>
2025-05-14 18:42:58 +0200

    hashtag model

---
+++ models/hashtag.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const hashtagSchema = new Schema({
+    name: {
+        type: String,
+        required: true,
+        unique: true,
+        lowercase: true,
+        trim: true
+    },
+    posts: [{
+        type: Schema.Types.ObjectId,
+        ref: 'post'
+    }],
+    count: {
+        type: Number,
+        default: 0
+    }
+},
+    { timestamps: true }
+);
+
+module.exports = mongoose.model('hashtag', hashtagSchema);
