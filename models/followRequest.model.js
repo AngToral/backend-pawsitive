b8dce9598f7e299d4ea7921fa686f8c8c58309f6
Angela Toral <158821520+AngToral@users.noreply.github.com>
2025-05-14 18:43:56 +0200

    follow request model

---
+++ models/followRequest.model.js
@@ -0,0 +1,24 @@
+const mongoose = require('mongoose');
+const Schema = mongoose.Schema;
+
+const followRequestSchema = new Schema({
+    from: {
+        type: Schema.Types.ObjectId,
+        ref: 'userModel',
+        required: true
+    },
+    to: {
+        type: Schema.Types.ObjectId,
+        ref: 'userModel',
+        required: true
+    },
+    status: {
+        type: String,
+        enum: ['pending', 'accepted', 'rejected'],
+        default: 'pending'
+    }
+}, {
+    timestamps: true
+});
+
+module.exports = mongoose.model('followRequest', followRequestSchema);
