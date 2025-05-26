e3fdddf752ebc013c7177c00cf1dbfbec90d8296
Angela Toral <158821520+AngToral@users.noreply.github.com>
2025-05-26 19:13:32 +0200

    user routes

--- routes/userRoutes.js
+++ routes/userRoutes.js
@@ -1,10 +1,18 @@
 const express = require("express");
-const { getUsers, } = require("../controllers/userController");
+const { getUsers, getUserId, updateUser, updatePhoto, registerUser, login, deleteUser, forgotPasswordEmail, sendChangePassword, } = require("../controllers/userController");
 const multer = require('multer');
 
 const userRouter = express.Router();
 const ProfilePicUpload = multer({ dest: './images-profile' })
 
 userRouter.get('/', getUsers)
+userRouter.get('/:id?', getUserId)
+userRouter.put('/:id?', updateUser)
+userRouter.put('/update/:id?', ProfilePicUpload.single('profilePic'), updatePhoto)
+userRouter.post('/register', registerUser)
+userRouter.post('/login', login)
+userRouter.delete('/:id?', deleteUser)
+userRouter.post('/forgottenpassword', forgotPasswordEmail) // Si se olvida la contraseña fuera del login
+userRouter.post('/changepassword', sendChangePassword) //Si quieres cambiar la contraseña dentro del login
 
 module.exports = { userRouter }
